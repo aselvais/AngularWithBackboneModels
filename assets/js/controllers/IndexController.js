569989e1b5673c7d3111b6731e4ee0b039734b33
aselvais@hostopia.com <aselvais@hostopia.com>
2016-04-21 16:16:18 -0400

    dirty parse HTML messages

--- assets/js/controllers/IndexController.js
+++ assets/js/controllers/IndexController.js
@@ -3,8 +3,25 @@
  * @param $scope
  * @constructor
  */
-angular.module('webmailApp', []).controller('IndexController', function ($scope) {
-    var emailmessageCollection = new EmailmessageCollection();
-    emailmessageCollection.initCollection();
-    $scope.emailmessageCollection = emailmessageCollection;
-});
+angular.module('webmailApp', []).controller('IndexController', ['$scope', '$sce', function ($scope, $sce)
+{
+	// init scope vars
+	$scope.htmlBody = "";
+	$scope.htmlBodyMessage = "";
+	$scope.emailmessageCollection;
+
+	// init models
+	var emailmessageCollection = new EmailmessageCollection();
+	emailmessageCollection.initCollection();
+	$scope.emailmessageCollection = emailmessageCollection;
+
+	/**
+	 *
+	 */
+	$scope.selectThisMessage = function(message)
+	{
+		emailmessageCollection.selectMessage(message);
+		$scope.htmlBodyMessage = $sce.trustAsHtml(emailmessageCollection.getSelected().get('body'));
+	}
+
+}]);
